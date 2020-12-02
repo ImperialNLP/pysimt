013ddf6af275ac0274f10b49d92b40ffbbe90f3d
Ozan Caglayan <ozancag@gmail.com>
2020-12-02 12:38:14 +0000

    pysimt: add module docstring, disable doc generation for some modules

--- pysimt/__init__.py
+++ pysimt/__init__.py
@@ -1,1 +1,20 @@
+"""
+`pysimt` is a `PyTorch`-based sequence-to-sequence (S2S) framework that facilitates
+research in unimodal and multi-modal machine translation. The framework
+is especially geared towards a set of recent simultaneous MT approaches, including
+heuristics-based decoding and prefix-to-prefix training/decoding. Common metrics
+such as average proportion (AP), average lag (AL), and consecutive wait (CW)
+are provided through well-defined APIs as well.
+
+
+.. include:: ./docs.md
+"""
+
 __version__ = '1.0.0'
+
+# Disable documentation generation for the following sub modules
+__pdoc__ = {
+    'pysimt.cocoeval': False,
+    'pysimt.config': False,
+    'pysimt.logger': False,
+}
