b856d1745ba5823ee648b3fd4f52d48603016d62
Ozan Caglayan <ozancag@gmail.com>
2020-12-10 18:53:07 +0000

    fusion: add docs

--- pysimt/layers/fusion.py
+++ pysimt/layers/fusion.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 import operator
+from typing import Optional
 from functools import reduce
 
 import torch
@@ -9,31 +9,40 @@ from ..utils.nn import get_activation_fn
 
 
 class Fusion(torch.nn.Module):
-    """A convenience fusion layer that merges an arbitrary number of inputs.
+    """A convenience layer that merges an arbitrary number of inputs using
+    concatenation, addition or multiplication. It then applies an optional
+    non-linearity given by the `activ` argument. If `operation==concat`,
+    additional arguments should be provided to define an adaptor MLP
+    that will project the concatenated vector into a lower dimensional space.
 
-    Arguments:
-        fusion_type(str, optional): One of ``concat,sum,mul`` defining the
-            fusion operation. In the default setup of ``concat``, the
-            following two arguments should be provided to create a
-            ``Linear`` adaptor which will project the concatenated vector to
-            ``output_size``.
-        input_size(int, optional): The dimensionality of the concatenated
-            input. Only necessary if ``fusion_type==concat``.
-        output_size(int, optional): The output dimensionality of the
-            concatenation. Only necessary if ``fusion_type==concat``.
+    Args:
+        operation: `concat`, `sum` or `mul` for concatenation, addition, and
+            multiplication respectively
+        activ: The activation function name that will be searched
+            in `torch` and `torch.nn.functional` modules. `None` or `linear`
+            disables the activation function
+        input_size: Only required for `concat` fusion, to denote the concatenated
+            input vector size. This will be used to add an MLP adaptor layer
+            after concatenation to project the fused vector into a lower
+            dimension
+        output_size: Only required for `concat` fusion, to denote the
+            output size of the aforementioned adaptor layer
     """
-
-    def __init__(self, fusion_type='concat', input_size=None, output_size=None,
-                 fusion_activ=None):
+    def __init__(self,
+                 operation: str = 'concat',
+                 activ: Optional[str] = 'linear',
+                 input_size: Optional[int] = None,
+                 output_size: Optional[int] = None):
+        """"""
         super().__init__()
 
-        self.fusion_type = fusion_type
-        self.fusion_activ = fusion_activ
-        self.forward = getattr(self, '_{}'.format(self.fusion_type))
-        self.activ = get_activation_fn(fusion_activ)
+        self.operation = operation
+        self.activ = activ
+        self.forward = getattr(self, '_{}'.format(self.operation))
+        self.activ = get_activation_fn(activ)
         self.adaptor = lambda x: x
 
-        if self.fusion_type == 'concat' or input_size != output_size:
+        if self.operation == 'concat' or input_size != output_size:
             self.adaptor = FF(input_size, output_size, bias=False, activ=None)
 
     def _sum(self, inputs):
@@ -46,6 +55,4 @@ class Fusion(torch.nn.Module):
         return self.activ(self.adaptor(torch.cat(inputs, dim=-1)))
 
     def __repr__(self):
-        return "Fusion(type={}, activ={})".format(
-            self.fusion_type,
-            self.fusion_activ)
+        return f"Fusion(type={self.operation}, activ={self.activ})"
